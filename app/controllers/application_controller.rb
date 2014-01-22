e3c603b78d807c2b70f0715ceca80ca154319905
rohan1020 <rohanraja9@gmail.com>
2014-01-22 19:05:41 +0530

    Added Discuss comments and syntax highlighter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def coderay(text)
+  		text.gsub(/\<code( language="(.+?)")?\>(.+?)\<\/code\>/m) do
+    #	content_tag("notextile", CodeRay.scan($3, $2).div(:css => :class))
+
+    		CodeRay.scan($3, $2).div(:css => :class)
+  		end
+
+	end
+
+
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,13 @@
 class HomeController < ApplicationController
   def index
-  	@post = Post.find(1)
+  	@post = Post.find(2)
   	@title = @post.title
   end
 
   def post
 
   	@post = Post.find(1)
+  	@post.body = coderay(@post.body)
   	@title = @post.title
 
   end
